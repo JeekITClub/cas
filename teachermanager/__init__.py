66256ed7b43c9ebcca768db26c6631f5b15ec2ab
AlienMao <qiushimao@hotmail.com>
2017-01-30 11:48:01 +0800

    教师管理 package
    未来设想

---
+++ teachermanager/__init__.py
@@ -0,0 +1,12 @@
+#Package for teacher management
+class teacher:
+    def add_teacher(self,name,subject):
+        pass
+    def delete_teacher(self,name):
+        pass
+    def edit_teacher(self,name):
+        pass
+    def search_teacher(self,name):
+        pass
+    def arrange_teacher(self,name):
+        pass
