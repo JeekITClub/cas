12eab4076d4c03484465ddddf20f09b2520179ca
AlienMao <qiushimao@hotmail.com>
2017-02-01 09:07:09 +0800

    cas
    cas

---
+++ cas/__init__.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+#Package for course management
+class course:
+    def arrange(self):
+        #用于安排课表
+        pass
+    def generate(self,name):
+        #生成课表函数
+        #分教师和学生课表
+        pass
+
+
